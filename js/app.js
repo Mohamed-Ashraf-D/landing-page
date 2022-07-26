9863a22736411a40b763bf5dec6e2df46cc5ba70
Mohamed Ashraf Mohamed <44390794+mido777283@users.noreply.github.com>
2022-07-26 21:22:40 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,98 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const list=document.getElementById('navbar__list');
+const sections=document.querySelectorAll('section');
+const fragment=document.createDocumentFragment();
+
+// create navbar
+sections.forEach((section)=>{
+const li=document.createElement('li');
+const link =document.createElement('a');
+const title =section.getAttribute('data-nav');
+const id=section.getAttribute('id');
+link.href=`#${id}`;
+link.textContent=title;
+link.classList.add('menu__link');
+link.addEventListener('click',(e)=>{
+    e.preventDefault();
+    section.scrollIntoView({
+        behavior:'smooth',
+    });
+})
+li.appendChild(link);
+fragment.appendChild(li);
+})
+list.appendChild(fragment);
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+//make active and highlight link
+window.addEventListener("scroll",()=>{
+    sections.forEach(section=>{
+        const top=section.getBoundingClientRect().top;
+        const link=document.querySelector(`a[href="#${section.id}"]`);
+        if(top>0 && top<250){
+            section.classList.add('your-active-class');
+            link.classList.add('active');
+        }
+        else{
+            section.classList.remove('your-active-class');
+            link.classList.remove('active');
+        }
+    })
+})
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
